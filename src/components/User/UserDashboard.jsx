0cfb30fa95f7f557bf3f217305ace0e1244de2b7
AvinashRaut1998 <avinashraut2498@gmail.com>
2024-01-09 17:14:13 +0530

    user components added

--- src/components/User/UserDashboard.jsx
+++ src/components/User/UserDashboard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import NavBar from "../NavBar";
+import { Box, CssBaseline, Toolbar } from "@mui/material";
+import UserDrawer from "./UserDrawer";
+import { Outlet } from "react-router-dom";
 
 const UserDashboard = () => {
-  return <div>UserDashboard</div>;
+  return (
+    <>
+      <Box>
+        <NavBar />
+        <Box sx={{ display: "flex", bgcolor: "#eee" }}>
+          <CssBaseline />
+          <UserDrawer />
+          <Box component="main" sx={{ flexGrow: 1, p: 3, minHeight: "100vh" }}>
+            <Toolbar />
+            <Outlet />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default UserDashboard;
